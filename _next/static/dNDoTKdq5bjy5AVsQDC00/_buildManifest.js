fa91cecf3b203772a21dbcab13f809c00375dfb8
sahiloco <>
2024-07-03 13:30:49 +0000

    Updates

---
+++ _next/static/dNDoTKdq5bjy5AVsQDC00/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-9e6e28659be79353.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],"/support":["static/chunks/pages/support-b425fe27ebb45050.js"],sortedPages:["/","/_app","/_error","/support"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
